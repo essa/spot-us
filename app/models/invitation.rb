b900551db939962cdb2f962b528ed5d998667cab
Taku <tnakajima@brain-tokyo.jp>
2008-11-07 13:41:44 +0900

    parsing email addresses

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,12 +1,22 @@
 class Invitation
   include Validateable
 
-  attr_accessor :sender, :news_item, :emails, :message
+  attr_reader :emails, :email_addresses_text
+  attr_accessor :sender, :news_item, :message
 
   validates_presence_of     :sender, :news_item
-  validates_length_of       :emails,    :within => 3..1000
-  validates_format_of       :emails, :with => %r{(\S*)@(\S*)\s*(,\s*(\S*)@(\S*))*}, :allow_blank => false
+  validates_length_of       :email_addresses_text, :within => 3..1000
 
+  UserName = '[^@\s]+'
+  DomainName = '(?:[-a-z0-9]+\.)+[a-z]{2,}'
+  EmailsRegexp = %r{\A\s*(#{UserName}@#{DomainName})\s*(,\s*(#{UserName}@#{DomainName}))*\s*\Z}
+
+  validates_format_of       :email_addresses_text, :with => EmailsRegexp, :allow_blank => false
+
+  def initialize
+    @email_addresses_text = nil
+    @emails = []
+  end
 
   def validate
     super
@@ -16,6 +26,20 @@ class Invitation
     end
     unless self.news_item.kind_of?(NewsItem)
       errors.add(:news_item, "is not a NewsItem")
+    end
+  end
+
+  def email_addresses_text=(text)
+    @email_addresses_text = text
+    parse_email_addresses
+  end
+
+  private
+
+  def parse_email_addresses
+    if @email_addresses_text =~ EmailsRegexp
+      p Regexp.last_match.to_a
+      @emails << Regexp.last_match(0)
     end
   end
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -7,20 +7,20 @@ describe Invitation do
     @news_item = Factory(:news_item, :user => @user)
   end
 
-  it 'has 4 attributes, sender, newsitem, emails and message'do
+  it 'has 4 attributes, sender, newsitem, email_addresses_text and message'do
     invitation = Invitation.new
     invitation.sender = @user
     invitation.news_item = @news_item
-    invitation.emails = 'tnakajima@brain-tokyo.jp'
+    invitation.email_addresses_text = 'tnakajima@brain-tokyo.jp'
     invitation.message = 'Check this'
     invitation.should be_valid
   end
 
-  it 'requires sender, emails and newsitem' do
+  it 'requires sender, email_addresses_text and newsitem' do
     invitation = Invitation.new
     invitation.should_not be_valid
     invitation.errors.on(:sender).should_not be_nil
-    invitation.errors.on(:emails).should_not be_nil
+    invitation.errors.on(:email_addresses_text).should_not be_nil
     invitation.errors.on(:news_item).should_not be_nil
   end
 
@@ -36,6 +36,48 @@ describe Invitation do
     invitation.news_item = 'should be NewsItem'
     invitation.should_not be_valid
     invitation.errors.on(:news_item).should_not be_nil
+  end
+
+  describe 'parsing email addresses' do
+    it 'can detect error on email_addresses_text' do
+      error_addresses = [
+                         'tnakajima@brain-tokyo.jp,@a',
+                         '',
+                         'tnakajima',
+                         'tnakajima@',
+                         '@brain-tokyo.jp',
+                         'tnakajima@brain-tokyo.jp,',
+                         'tnakajima@brain-tokyo.jp,a',
+                        ]
+      error_addresses.each do |a|
+        begin
+          invitation = make_invitation
+          invitation.email_addresses_text = a
+          invitation.should_not be_valid
+          invitation.errors.on(:email_addresses_text).should_not be_nil
+        rescue
+          STDERR.puts "an exception raised at parsing #{a}"
+          p Regexp.last_match
+          raise
+        end
+      end
+    end
+
+    it 'can parse one email_addresse' do
+      invitation = make_invitation
+      invitation.email_addresses_text = 'tnakajima@brain-tokyo.jp'
+      invitation.should be_valid
+      invitation.should have(1).emails
+      invitation.emails[0].should == 'tnakajima@brain-tokyo.jp'
+    end
+
+    def make_invitation
+      invitation = Invitation.new
+      invitation.sender = @user
+      invitation.news_item = @news_item
+      invitation.message = 'Check this'
+      invitation
+    end
   end
 end
 
